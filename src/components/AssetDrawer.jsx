277c3e7325bc1ae630ec073e137343db56a38178
thediaryofastudentleader <thediaryofastudentleader@gmail.com>
2026-06-23 21:08:08 +0200

    Refactor AssetDrawer component and export default

--- src/components/AssetDrawer.jsx
+++ src/components/AssetDrawer.jsx
@@ -1,22 +1,25 @@
 import React from "react";
 import { IngredientIcon } from "./IngredientIcon";
 
-export const AssetDrawer = () => {
-    const items = [
-        "Tomato Sauce",
-        "Mozzarella",
-        "Shredded Chicken",
-        "Sliced Mushrooms",
-        "Ground Beef",
-        "Pineapple",
-        "Pepperoni",
-    ];
-    return (
-        <div className="space-y-1">
-            <h2 className="font-bold text-lg mb-3 text-center">Ingredients</h2>
-            {items.map((i) => (
-                <IngredientIcon key={i} name={i} />
-            ))}
-        </div>
-    );
+const AssetDrawer = () => {
+  const items = [
+    "Tomato Sauce",
+    "Mozzarella",
+    "Shredded Chicken",
+    "Sliced Mushrooms",
+    "Ground Beef",
+    "Pineapple",
+    "Pepperoni",
+  ];
+  return (
+    <div className="space-y-1">
+      <h2 className="font-bold text-lg mb-3 text-center">Ingredients</h2>
+      {items.map((i) => (
+        <IngredientIcon key={i} name={i} />
+      ))}
+    </div>
+  );
 };
+
+export default AssetDrawer;
+
